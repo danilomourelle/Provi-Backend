e00e8ccecc9111622582ebf4d29031a9380df08a
Danilo Mourelle <danilomourelle@outlook.com>
2020-07-27 18:59:25 -0300

    Phone Database

--- src/data/NameDatabase.ts
+++ src/data/NameDatabase.ts
@@ -15,4 +15,24 @@ export class NameDatabase extends BaseDatabase {
       )
     )
   }
+
+  
+  public async getNameByValue(value: string): Promise<Name | undefined> {
+    const result = await super.getConnection()
+    .select("*")
+    .from(NameDatabase.TABLE_NAME)
+    .where({first_name: value})
+    .orWhere({last_name: value})
+
+    return this.toModel(result[0])
+  }
+
+  public async getNameByUserId(id: string): Promise<Name | undefined>{
+    const result = await super.getConnection()
+    .select("*")
+    .from(NameDatabase.TABLE_NAME)
+    .where({user_id: id})
+
+    return this.toModel(result[0])
+  }
 }
